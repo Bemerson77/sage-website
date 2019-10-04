072123d26cc25efecec1e3ea803d365bf6ad04f0
bemerson <bemerson@gracehill.com>
2019-10-03 22:14:32 -0400

    Updates

---
+++ precache-manifest.57509ec7f9dd014be3ef69f2d6089347.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e233c31ca13322cfef32ea243950326",
+    "url": "/sage-website/index.html"
+  },
+  {
+    "revision": "118d61f81d51dd669136",
+    "url": "/sage-website/static/css/main.584d6dba.chunk.css"
+  },
+  {
+    "revision": "113e5f01fd5d9f72e0d3",
+    "url": "/sage-website/static/js/2.e7300a2b.chunk.js"
+  },
+  {
+    "revision": "118d61f81d51dd669136",
+    "url": "/sage-website/static/js/main.e2030b37.chunk.js"
+  },
+  {
+    "revision": "cf8d6de4338b2ba4695e",
+    "url": "/sage-website/static/js/runtime-main.5b11a5ca.js"
+  },
+  {
+    "revision": "fa9b0a97fe182e6b50fe023fdf2d454f",
+    "url": "/sage-website/static/media/SageSalon105.fa9b0a97.png"
+  },
+  {
+    "revision": "50df172f3e0ca6a40934b7302648eff7",
+    "url": "/sage-website/static/media/sage-logo.50df172f.png"
+  }
+]);
