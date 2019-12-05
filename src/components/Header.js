306231f5c1249bd705eb710069b6ef79a49324f6
bemerson <bemerson@gracehill.com>
2019-12-04 19:27:08 -0500

    logo and nav in place

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function HeaderComponent (){
     <div className="header-outer">
       <div className="header-inner">
         <div className="logo-wrapper">
-          
+          <img src={sageLogo} className="sage-logo" alt="logo" />
         </div>
         <ul className="nav-link-outer">
           <li className="nav-link-item">
