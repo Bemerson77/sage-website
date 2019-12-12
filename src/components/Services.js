bbbd45791a4e431fe8b6f2ccd1ce31686262565a
bemerson <bemerson@gracehill.com>
2019-12-12 00:14:32 -0500

    finished services page

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import '../App.scss';
 import salonImg from '../img/SageSalon108.jpg';
@@ -8,8 +14,221 @@ function ServicesComponent (){
   return (
     <div>
       <BannerComponent imgPath={salonImg} />
+      <ServiceList />
     </div>
 
+  )
+}
+
+function ServiceList (){
+  return (
+    <div className="service-list-outer">
+      <Container maxWidth="md">
+        <Grid container direction={'row'} className="service-section-outer">
+          <Grid item xs={1}></Grid>
+          <Grid item xs={10}>
+            <h1 className="services-title">Sage Services</h1>
+            <p className="">All new guests will receive a free consultation with one of our specially trained stylists. Please confirm pricing with your stylist.</p>
+              <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel1a-content"
+                 id="panel1a-header"
+               >
+                <h3 className="service-sub-title">Hair Styling</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Haircut</p>
+                     <p className="service">Blowdry&#47;Finish</p>
+                     <p className="service">Childs Cut &#40;12 and under&#41;</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel2a-content"
+                 id="panel2a-header"
+               >
+                <h3 className="service-sub-title">Color</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Glaze&#47;Demi &#8211; Permanent</p>
+                     <p className="service">Single Process</p>
+                     <p className="service">Highlight &#40;full head&#41;</p>
+                     <p className="service">Highlight &#40;partial&#41;</p>
+                     <p className="service">Single Process with Highlight</p>
+                     <p className="service">Balayage</p>
+                     <p className="service">Hair Painting</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+               <h3 className="service-sub-title">Mens</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Cut</p>
+                     <p className="service">Color</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+               <h3 className="service-sub-title">Keratin Treatment&#47;Relaxers</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Keratin Blowout</p>
+                     <p className="service">Keratin</p>
+                     <p className="service">Relaxer</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+               <h3 className="service-sub-title">Hair Extensions</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">By appointment only</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+               <h3 className="service-sub-title">Permanent Waves</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Perm</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+               <h3 className="service-sub-title">Weddings and Special Events</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">By appointment only</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+                 <h3 className="service-sub-title">Makeup</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Application</p>
+                     <p className="service">Lash Application</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <ExpansionPanel>
+               <ExpansionPanelSummary
+                 expandIcon={<ExpandMoreIcon />}
+                 aria-controls="panel3a-content"
+                 id="panel3a-header"
+               >
+               <h3 className="service-sub-title">Waxing Services</h3>
+               </ExpansionPanelSummary>
+               <ExpansionPanelDetails>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Brows</p>
+                     <p className="service">Lip</p>
+                     <p className="service">Chin</p>
+                 </Grid>
+               </ExpansionPanelDetails>
+             </ExpansionPanel>
+             <h1 className="services-title">Lashes at Sage</h1>
+               <ExpansionPanel>
+                 <ExpansionPanelSummary
+                   expandIcon={<ExpandMoreIcon />}
+                   aria-controls="panel3a-content"
+                   id="panel3a-header"
+                 >
+                   <h3 className="service-sub-title">Lash Extensions</h3>
+                 </ExpansionPanelSummary>
+                 <ExpansionPanelDetails>
+                   <Grid item xs={12} className="service-section">
+                       <p className="service">Classic Lashes</p>
+                       <p className="service">Hybrid Lashes</p>
+                       <p className="service">Volume Lashes</p>
+                       <p className="service">Classic Fill</p>
+                       <p className="service">Volume Fill</p>
+                   </Grid>
+                 </ExpansionPanelDetails>
+               </ExpansionPanel>
+               <ExpansionPanel>
+                 <ExpansionPanelSummary
+                   expandIcon={<ExpandMoreIcon />}
+                   aria-controls="panel3a-content"
+                   id="panel3a-header"
+                 >
+                   <h3 className="service-sub-title">Waxing Services</h3>
+                 </ExpansionPanelSummary>
+                 <ExpansionPanelDetails>
+                   <Grid item xs={12} className="service-section">
+                       <p className="service">Brow Wax</p>
+                       <p className="service">Brow Wax and Tint</p>
+                   </Grid>
+                 </ExpansionPanelDetails>
+               </ExpansionPanel>
+               <ExpansionPanel>
+                 <ExpansionPanelSummary
+                   expandIcon={<ExpandMoreIcon />}
+                   aria-controls="panel3a-content"
+                   id="panel3a-header"
+                 >
+                   <h3 className="service-sub-title">Tanning Services</h3>
+                 </ExpansionPanelSummary>
+                 <ExpansionPanelDetails>
+                   <Grid item xs={12} className="service-section">
+                       <p className="service">Spray Tan</p>
+                   </Grid>
+                 </ExpansionPanelDetails>
+               </ExpansionPanel>
+               <h1 className="services-title">Products</h1>
+                 <Grid item xs={12} className="service-section">
+                     <p className="service">Eufora</p>
+                     <p className="service">Redkin Color</p>
+                     <p className="service">Unite</p>
+                 </Grid>
+          </Grid>
+          <Grid item xs={1}></Grid>
+        </Grid>
+
+      </Container>
+    </div>
   )
 }
 
--- src/components/Team.js
+++ src/components/Team.js
@@ -3,57 +3,140 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 
 import '../App.scss';
-import teamData from '../data/team-data.json';
 import salonImg from '../img/SageSalon117.jpg';
+import brittanyImg from '../img/SageSalon24.jpg';
+import katieImg from '../img/SageSalon34.jpg';
+import alliImg from '../img/SageSalon45.jpg';
+import sageImg from '../img/SageSalon62.jpg';
+import amberImg from '../img/SageSalon17.jpg';
+import tonyaImg from '../img/SageSalon08.jpg';
+import mimiImg from '../img/SageSalon51.jpg';
+import carmenImg from '../img/SageSalon06.jpg';
+import hollyImg from '../img/SageSalon83.jpg';
 import maryImg from '../img/SageSalon38.jpg';
 import BannerComponent from './Banner';
 
-function TeamComponent ({ teamData }){
+function TeamComponent (){
 
 
  return (
   <div>
     <BannerComponent imgPath={salonImg} />
     <div className="stylist-outer">
-      <StylistComponent teamData={teamData}/>
+      <StylistComponent />
     </div>
   </div>
 
  )
 }
 
-// const TeamMember = ({ teamData }) => (
-//   <ul>
-//     {teamData.map(item =>{
-//       console.log(item)
-//       return <div>{item}</div>;
-//     }
-//   )}
-//   </ul>
-// );
-
 
 function StylistComponent() {
-  var commentNodes = teamData.map(function (member, i){
-    console.log(member.teamMember);
-        return (
-          <Grid item xs={6} className="stylist-img-outer" key={i}>
-            <div className="team-member-outer">
-              <div className="team-member" style={{ background: `url(${member.teamMember.imageTag}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
-                <div className="txt-wrapper">
-                  <h1 className="team-name">{member.teamMember.name}</h1>
-                  <h3 className="team-title">{member.teamMember.position}</h3>
-                </div>
-              </div>
-            </div>
-          </Grid>
-        );
-    });
+
   return (
     <div>
       <Container maxWidth="md">
         <Grid container direction={'row'}>
-        {commentNodes}
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${hollyImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Holly Chapman</h1>
+                <h3 className="team-title"> Owner and Stylist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${sageImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Sage Chapman</h1>
+                <h3 className="team-title">Stylist and Makeup Artist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${brittanyImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Brittany Porter</h1>
+                <h3 className="team-title">Stylist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${alliImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Alli Taylor</h1>
+                <h3 className="team-title">Stylist and Makeup Artist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${katieImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Katie Broome</h1>
+                <h3 className="team-title">Stylist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${maryImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Mary Emerson</h1>
+                <h3 className="team-title">Stylist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${amberImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Amber Trull</h1>
+                <h3 className="team-title">Stylist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${mimiImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Mimi Nguyen</h1>
+                <h3 className="team-title">Lash Artist and Stylist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${tonyaImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Tonya Taylor</h1>
+                <h3 className="team-title">Receptionist and Spray Tans</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="stylist-img-outer">
+          <div className="team-member-outer">
+            <div className="team-member" style={{ background: `url(${carmenImg}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
+              <div className="txt-wrapper">
+                <h1 className="team-name">Carmen Lopez</h1>
+                <h3 className="team-title">Lash Artist</h3>
+              </div>
+            </div>
+          </div>
+        </Grid>
       </Grid>
       </Container>
 
