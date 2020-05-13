fa82e5b0acb7bfcb086ba11caa39eca162821edd
bemerson <bemerson@gracehill.com>
2020-05-13 17:12:49 -0400

    removed alert

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,9 +11,9 @@ function BannerComponent ({ imgPath }){
       <div className="banner" style={{ background: `url(${imgPath}) center center`, backgroundSize: 'cover', backgroundColor: 'rgba(233, 232, 234, 1)' }}>
 
       </div>
-      <div className="alert-outer">
+      {/*<div className="alert-outer">
         <h3 className="alert">We are closed temporarily due to COVID-19 pending an opening date per the Governor.</h3>
-      </div>
+      </div>*/}
       {/*<FontAwesomeIcon icon={Icons.faChevronDown} className="down-chevron"/>*/}
     </div>
   )
